ca1104308b4c09e5fcacde47c4b83992b2e01ad9
Honestyan Didyafarhan <honestyan0708@gmail.com>
2024-10-03 17:16:21 +0700

    undefined

--- out/config.js
+++ out/config.js
@@ -2,18 +2,26 @@ import fs from "fs";
 import path from "path";
 import os from "os";
 const configFilePath = path.join(os.homedir(), ".malas-commit");
-let loadedConfig = {};
+const defaultConfig = {
+    GROQ_APIKEY: "",
+    COMMIT_PROMPT: "",
+};
 const loadConfig = () => {
     try {
         const configFile = fs.readFileSync(configFilePath, "utf-8");
         return JSON.parse(configFile);
     }
     catch (err) {
-        const errorMessage = `Failed to load config from ${configFilePath}: ${err instanceof Error ? err.message : String(err)}\nRun 'malas setConfig GROQ_APIKEY <your-apikey>'`;
-        throw new Error(errorMessage);
+        console.warn(`Failed to load config from ${configFilePath}: ${err instanceof Error ? err.message : String(err)}\n\n\n Run 'malas setConfig GROQ_APIKEY <your_apikey>' \n\n\n`);
+        saveConfig(defaultConfig);
+        return defaultConfig;
     }
 };
-loadedConfig = loadConfig();
+const saveConfig = (config) => {
+    fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2), "utf-8");
+    console.log(`Configuration file created at: ${configFilePath}`);
+};
+const loadedConfig = loadConfig();
 export const config = {
     GROQ_APIKEY: process.env.GROQ_APIKEY || loadedConfig.GROQ_APIKEY || "",
     COMMIT_PROMPT: process.env.COMMIT_PROMPT || loadedConfig.COMMIT_PROMPT || "",
--- src/config.ts
+++ src/config.ts
@@ -3,27 +3,40 @@ import path from "path";
 import os from "os";
 
 const configFilePath = path.join(os.homedir(), ".malas-commit");
+
 interface Config {
   GROQ_APIKEY?: string;
   COMMIT_PROMPT?: string;
 }
 
-let loadedConfig: Config = {};
+const defaultConfig: Config = {
+  GROQ_APIKEY: "",
+  COMMIT_PROMPT: "",
+};
 
-const loadConfig = () => {
+const loadConfig = (): Config => {
   try {
     const configFile = fs.readFileSync(configFilePath, "utf-8");
     return JSON.parse(configFile);
   } catch (err) {
-    const errorMessage = `Failed to load config from ${configFilePath}: ${
-      err instanceof Error ? err.message : String(err)
-    }\nRun 'malas setConfig GROQ_APIKEY <your-apikey>'`;
+    console.warn(
+      `Failed to load config from ${configFilePath}: ${
+        err instanceof Error ? err.message : String(err)
+      }\n\n\n Run 'malas setConfig GROQ_APIKEY <your_apikey>' \n\n\n`
+    );
 
-    throw new Error(errorMessage);
+    saveConfig(defaultConfig);
+
+    return defaultConfig;
   }
 };
 
-loadedConfig = loadConfig();
+const saveConfig = (config: Config) => {
+  fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2), "utf-8");
+  console.log(`Configuration file created at: ${configFilePath}`);
+};
+
+const loadedConfig: Config = loadConfig();
 
 export const config = {
   GROQ_APIKEY: process.env.GROQ_APIKEY || loadedConfig.GROQ_APIKEY || "",
