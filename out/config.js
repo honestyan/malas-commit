882180ec46bdfc6f4e84040bfa6db988de01a35a
Honestyan Didyafarhan <honestyan0708@gmail.com>
2024-10-05 02:23:42 +0700

    Here's a proposed commit message:
    [Update] config.js and index.js files updated with bug fixes and minor changes to improve functionality.

--- out/config.js
+++ out/config.js
@@ -3,31 +3,30 @@ import path from "path";
 import os from "os";
 const configFilePath = path.join(os.homedir(), ".malas-commit");
 const defaultConfig = {
-  GROQ_APIKEY: "",
-  COMMIT_PROMPT: "",
+    GROQ_APIKEY: "",
+    COMMIT_PROMPT: "",
 };
 const loadConfig = () => {
-  try {
-    const configFile = fs.readFileSync(configFilePath, "utf-8");
-    return JSON.parse(configFile);
-  } catch {
-    console.log(`Configuration file not found at ${configFilePath}.\n`);
-    console.log(`Creating a new configuration file...\n`);
-    saveConfig(defaultConfig);
-    console.log(
-      `A new configuration file has been created at ${configFilePath}.\n`
-    );
-    console.log(`Please update the configuration by running:\n`);
-    console.log(`'malas setConfig GROQ_APIKEY <your_apikey>'\n`);
-    process.exit(1);
-    return defaultConfig;
-  }
+    try {
+        const configFile = fs.readFileSync(configFilePath, "utf-8");
+        return JSON.parse(configFile);
+    }
+    catch {
+        console.log(`Configuration file not found at ${configFilePath}.\n`);
+        console.log(`Creating a new configuration file...\n`);
+        saveConfig(defaultConfig);
+        console.log(`A new configuration file has been created at ${configFilePath}.\n`);
+        console.log(`Please update the configuration by running:\n`);
+        console.log(`'malas setConfig GROQ_APIKEY <your_apikey>'\n`);
+        process.exit(1);
+        return defaultConfig;
+    }
 };
 const saveConfig = (config) => {
-  fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2), "utf-8");
+    fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2), "utf-8");
 };
 const loadedConfig = loadConfig();
 export const config = {
-  GROQ_APIKEY: process.env.GROQ_APIKEY || loadedConfig.GROQ_APIKEY || "",
-  COMMIT_PROMPT: process.env.COMMIT_PROMPT || loadedConfig.COMMIT_PROMPT || "",
+    GROQ_APIKEY: process.env.GROQ_APIKEY || loadedConfig.GROQ_APIKEY || "",
+    COMMIT_PROMPT: process.env.COMMIT_PROMPT || loadedConfig.COMMIT_PROMPT || "",
 };
